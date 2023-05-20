5935bd532c7ffff0e7d6b70617eb086ccba1703f
elbert timothy thomas <elberthomax@gmail.com>
2023-05-20 16:44:32 +0800

    added basic detail page

--- models/campground.js
+++ models/campground.js
@@ -10,20 +10,25 @@ const campgroundSchema = new mongoose.Schema({
 const Campground = mongoose.model("Campground", campgroundSchema);
 
 async function getAllCampgrounds(){
-    const campgrounds = await Campground.find({})
-    return campgrounds
+    const campgrounds = await Campground.find({});
+    return campgrounds;
+}
+
+async function getCampgroundById(id){
+    const campground = await Campground.findById(id);
+    return campground;
 }
 
 async function createCampground(newData){
-    const newCampground = Campground(newData)
-    const result = await newCampground.save()
-    return result
+    const newCampground = Campground(newData);
+    const result = await newCampground.save();
+    return result;
 }
 
 async function deleteAll(){
     await Campground.deleteMany({});
 }
 
-const campGroundModel = {getAllCampgrounds, createCampground, deleteAll}
+const campGroundModel = {getAllCampgrounds, getCampgroundById, createCampground, deleteAll}
 
 module.exports = campGroundModel
--- routes/campground.js
+++ routes/campground.js
@@ -5,13 +5,24 @@ const router = express.Router()
 router.get("/", async (req, res) => {
     try {
         const campgrounds = await campgroundModel.getAllCampgrounds()
-        res.render("index", {campgrounds: campgrounds})
+        res.render("index", { campgrounds })
     }catch(err){
         console.log(err.message)
         res.render("error")
     }
 })
 
-
+router.get("/:id", async (req, res) => {
+        try{
+            const id = req.params.id;
+            const campground = await campgroundModel.getCampgroundById(id)
+            if(campground){
+                res.render("detail", { campground })
+            }else res.status(404).render("notfound")
+        }catch(err){
+            console.log(err.message)
+            res.render("error")
+        }
+})
 
 module.exports = router
